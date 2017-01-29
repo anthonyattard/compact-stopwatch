0dc4acbb47e9d22d8d8b8ca37a7e4c875769fb5a
Anthony Attard <anthony@anthonyattard.com>
2017-01-28 17:14:13 -0800

    Initial Commit

---
+++ StopwatchMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StopwatchMacOS
+//
+//  Created by Anthony on 1/28/17.
+//  Copyright © 2017 Anthony Attard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StopwatchMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StopwatchMacOS
+//
+//  Created by Anthony on 1/28/17.
+//  Copyright © 2017 Anthony Attard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
